4cabce4a71b039ad54cfa758190821cda45e8416
Bilol00 <xayrullayev288@gmail.com>
2024-03-02 15:14:11 +0500

    first commit

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../layots/navbar/Navbar'
+import Keep from './keep/Keep'
+function Home() {
+  return (
+    <div>
+      <Keep/>
+   <h1>adsas</h1>
+    </div>
+  )
+}
+
+export default Home
